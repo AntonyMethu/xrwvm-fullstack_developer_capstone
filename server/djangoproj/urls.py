187cd93fc77eb12d45f17a9fafefb403dc60b32e
Anthony Methu Kamau <122969529+AntonyMethu@users.noreply.github.com>
2026-09-18 18:57:28 +0000

    Implement dynamic pages, update React routing, and build frontend assets

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -1,7 +1,7 @@
 """djangoproj URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://django.readthedocs.io/en/stable/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -23,4 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
     path('', TemplateView.as_view(template_name="Home.html")),
+    path('dealers/', TemplateView.as_view(template_name="index.html")),
+    path('dealer/<int:dealer_id>', TemplateView.as_view(template_name="index.html")),
+    path('postreview/<int:dealer_id>', TemplateView.as_view(template_name="index.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

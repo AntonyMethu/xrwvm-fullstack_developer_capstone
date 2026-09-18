4fd7620ea6e52818a7dcbb4ae9542623e36eb9a1
Anthony Methu Kamau <122969529+AntonyMethu@users.noreply.github.com>
2026-09-18 20:17:59 +0000

    Fix React registration export syntax, backend API routing, and static build setup

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -11,26 +11,22 @@ sentiment_analyzer_url = os.getenv(
     default="http://localhost:5050/")
 
 def get_request(endpoint, **kwargs):
-    params = ""
-    if(kwargs):
-        for key, value in kwargs.items():
-            params = params + key + "=" + value + "&"
-
-    request_url = backend_url + endpoint + "?" + params
+    request_url = backend_url + endpoint
+    if kwargs:
+        params = "&".join([f"{key}={value}" for key, value in kwargs.items()])
+        request_url = f"{request_url}?{params}"
 
     print("GET from {} ".format(request_url))
     try:
-        # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
-        # If any error occurs
-        print("Network exception occurred")
+    except Exception as err:
+        print(f"Network exception occurred: {err}")
+        return []
 
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url + "analyze/" + text
     try:
-        # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
     except Exception as err:
@@ -43,5 +39,5 @@ def post_review(data_dict):
         response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except:
-        print("Network exception occurred")
+    except Exception as err:
+        print(f"Network exception occurred: {err}")
--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -2,16 +2,6 @@
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://django.readthedocs.io/en/stable/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path, include
@@ -22,7 +12,11 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
-    path('', TemplateView.as_view(template_name="Home.html")),
+    path('', TemplateView.as_view(template_name="home.html")),
+    path('about/', TemplateView.as_view(template_name="about.html")),
+    path('contact/', TemplateView.as_view(template_name="contact.html")),
+    path('login/', TemplateView.as_view(template_name="index.html")),
+    path('register/', TemplateView.as_view(template_name="index.html")),
     path('dealers/', TemplateView.as_view(template_name="index.html")),
     path('dealer/<int:dealer_id>', TemplateView.as_view(template_name="index.html")),
     path('postreview/<int:dealer_id>', TemplateView.as_view(template_name="index.html")),
